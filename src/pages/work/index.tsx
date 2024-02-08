75fab645e5354e72dac538cf747d6f52d9e00470
nurrizkyimani <nurrizkyimani@mail.ugm.ac.id>
2024-02-08 17:08:05 +0700

    feat: git init skeleton

---
+++ src/pages/work/index.tsx
@@ -0,0 +1,12 @@
+
+// Remove "use client" directive if present and keep the metadata export
+import type { Metadata } from 'next';
+
+
+export default function WorkPage() {
+    return (
+        <section>
+            <div>WorkPage</div>
+        </section>
+    );
+}
